12d3959b428fbf076f5a91b0079b9859a9e45027
moulik-1612 <mouliktiwari1612@gmail.com>
2026-03-11 16:15:26 +0530

    write a code to connect to server by just adding uri in env

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import { createServer } from "http";
 import app from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
+import dotenv from "dotenv";
 
 const server = createServer(app);
 
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+//connect db
+connectDB();
 
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
---
+++ src/config/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("✅ MongoDB connected successfully"))
+    .catch((err) => console.error("❌ MongoDB connection error:", err));
+};
